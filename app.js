ade13bd0942b727738a81b4d2e7c7654670e1fa9
Shuichi Tsukamoto <shtsukam@microsoft.com>
2017-09-06 13:48:42 +0900

    update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ console.log(process.env.port)
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 1337, function () {
     //console.log('%s listening to %s', server.name, server.url);
-    console.log('listening to server.name:%s server.url:%s', server.name, server.url)
+    console.log('listening to server.name:%s', server.name)
 });
 
 // Create chat connector for communicating with the Bot Framework Service
@@ -22,11 +22,12 @@ var connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
     appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
-
+console.log("server1")
 // Listen for messages from users 
 server.post('/api/messages', connector.listen());
-
+console.log("server2")
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 var bot = new builder.UniversalBot(connector, function (session) {
     session.send("You said: %s", session.message.text);
 });
+console.log("server3")
