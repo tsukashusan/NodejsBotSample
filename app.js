3083fd5cf16f053b506d0cb089edfd80fc7fbfaa
Shuichi Tsukamoto <shtsukam@microsoft.com>
2017-09-06 14:37:41 +0900

    update app.js

--- app.js
+++ app.js
@@ -28,5 +28,5 @@ server.post('/api/messages', connector.listen());
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 var bot = new builder.UniversalBot(connector, function (session) {
     console.log("You said: %s", session.message.text);
-    session.send("You said: %s", session.message.text);
+    session.send("貴方の会話: %s", session.message.text);
 });
